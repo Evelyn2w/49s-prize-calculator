f2f63f66818a7629684f6763078a6444c2ebaeb4
lottery Manager <133771589+Evelyn2w@users.noreply.github.com>
2025-07-31 14:33:36 +0500

    Create section.js

---
+++ section.js
@@ -0,0 +1,6 @@
+<script>
+function toggleTool() {
+  const section = document.getElementById("toolArea");
+  section.style.display = section.style.display === "none" ? "block" : "none";
+}
+</script>
